d2b58f155909d84c757866ea9f7cdf9e4b6dfb29
victor-gabriel-dev <victorg100vg@gmail.com>
2025-01-26 21:30:18 -0300

    ex de detran

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > log) { //Condição simples!
+        console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+    }
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Voce e brasileiro!')
+} else {
+    console.log('voce e Estrageiro')
+}
