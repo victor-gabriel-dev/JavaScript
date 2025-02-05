18d41757f0ee3588e58020ea7920ba704f151174
victor-gabriel-dev <victorg100vg@gmail.com>
2025-02-04 22:28:40 -0300

    javascript

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} Horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom Dia!
+        img.src = 'manha.png'
+        document.body.style.background = 'a1c4ee'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde!
+        img.src = 'tarde.png' 
+        document.body.style.background = 'fffe7f'
+    } else {
+        //Boa Noite!
+        img.src = 'noite.png' 
+        document.body.style.background = '2666a3'
+        }
+}
+
+
--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,49 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto' )
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade >=0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'crinça.jpg')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'jovem.jpg')
+            }
+            else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adulto.jpg')
+            }
+            else {
+                //Idoso
+                img.setAttribute('src', 'idosa.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+            if (idade >=0 && idade < 10){
+                //Criança
+            } else if (idade < 21){
+                //jovem
+            }
+            else if (idade < 50) {
+                //Adulto
+            }
+            else {
+                //Idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Agora descobri que é ${genero} do ano de ${idade}.`
+    }
+    res.appendChild(img)
+}
